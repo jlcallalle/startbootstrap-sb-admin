203b538decd8ccede7a62a1ba0bfa34052094545
Jorge Callalle <jlcallalle@gmail.com>
2022-03-15 21:15:24 -0500

    update builder theme modyo

---
+++ base-invex/main.js
@@ -0,0 +1,14 @@
+(function($) {
+    "use strict"; // Start of use strict
+  
+    // Toggle the side navigation
+    $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+      $("body").toggleClass("sidebar-toggled");
+      $(".sidebar").toggleClass("toggled");
+      if ($(".sidebar").hasClass("toggled")) {
+        $('.sidebar .collapse').collapse('hide');
+      }
+    });
+  
+  })(jQuery); // End of use strict
+  
